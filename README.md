a6bcd285d060eb83540ab9ced055064a7bb0c74f
Julyanderson Alves <ansderson84001@gmail.com>
2021-11-30 17:43:26 -0300

    jogo pronto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Descrição
+
+É um desafio proposto pela Digital Innovation One, através do bootcamp HTML Web Developer. Objetivo é criar um jogo da cobrinha usando Javascript.
--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
 }
 let direction = "right"
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)*box,
+    y: Math.floor(Math.random() * 15 + 1)*box
+}
+
 
 function criarBG(){
     context.fillStyle = "Lightgreen"
@@ -20,6 +25,11 @@ function criarCobrinha(){
         context.fillStyle = "green"
         context.fillRect(snake[i].x,snake[i].y,box,box)
     }
+}
+
+function drawfood(){
+    context.fillStyle = "red"
+    context.fillRect(food.x,food.y,box,box)
 }
 
 document.addEventListener('keydown',update)
@@ -36,11 +46,17 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
-    
-    
+
+    for(i= 1;i<snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+            alert("Game Over")
+        }
+    }
 
     criarBG();
     criarCobrinha();
+    drawfood();
    
 
     let snakeX = snake[0].x;
@@ -51,8 +67,13 @@ function iniciarJogo(){
     if (direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop(); 
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1)*box
+        food.y = Math.floor(Math.random() * 15 + 1)*box
+    }
     
-    snake.pop(); //pop tira o último elemento da lista
     
     
     let newHead ={
